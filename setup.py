be4a3fc4177be7ed602685f0cf00ca61a8e323c8
root <accerit@gmail.com>
2024-12-31 18:33:59 +0000

    Update dependencies and add setup.py

--- setup.py
+++ setup.py
@@ -1,75 +1,32 @@
 # Copyright (c) 2023 Microsoft
 # Licensed under The MIT License [see LICENSE for details]
 
-from setuptools import find_packages, setup
-
-# PEP0440 compatible formatted version, see:
-# https://www.python.org/dev/peps/pep-0440/
-#
-# release markers:
-#   X.Y
-#   X.Y.Z   # For bugfix releases
-#
-# pre-release markers:
-#   X.YaN   # Alpha release
-#   X.YbN   # Beta release
-#   X.YrcN  # Release Candidate
-#   X.Y     # Final release
-
-# version.py defines the VERSION and VERSION_SHORT variables.
-# We use exec here so we don't import allennlp whilst setting up.
-VERSION = {}  # type: ignore
-with open("promptwizard/version.py", "r") as version_file:
-    exec(version_file.read(), VERSION)
-
-INSTALL_REQUIRES = [
-    "datasets",
-    "tiktoken",
-    "nltk",
-    "openai",
-    "azure-identity",
-    "azure-search-documents",
-    "pyyaml~=6.0.1",
-    "pyarrow==15.0.2",
-    "llama-index==0.11.10",
-    "llama-index-core==0.11.10",
-    "python-dotenv"
-]
-QUANLITY_REQUIRES = [
-    "black==21.4b0",
-    "flake8>=3.8.3",
-    "isort>=5.5.4",
-    "pre-commit",
-    "pytest",
-    "pytest-xdist",
-]
-DEV_REQUIRES = INSTALL_REQUIRES + QUANLITY_REQUIRES
+from setuptools import setup, find_packages
 
 setup(
     name="promptwizard",
-    version=VERSION["VERSION"],
-    author="The PromptWizard team",
-    author_email="promptwizard@microsoft.com",
-    description="Optimize Prompt",
-    long_description=open("README.md", encoding="utf8").read(),
-    long_description_content_type="text/markdown",
-    keywords="PromptWizard",
-    license="MIT License",
-    url="https://github.com/microsoft/PromptWizard",
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "streamlit>=1.31.1",
+        "pandas>=2.2.0",
+        "python-dotenv>=1.0.1",
+        "google-cloud-aiplatform>=1.42.1",
+        "Pillow>=10.2.0",
+        "pyperclip>=1.8.2",
+        "llama-index-core>=0.10.1",
+        "google-generativeai>=0.3.2",
     ],
-    package_dir={"": "."},
-    packages=find_packages("."),
-    extras_require={
-        "dev": DEV_REQUIRES,
-        "quality": QUANLITY_REQUIRES,
-    },
-    install_requires=INSTALL_REQUIRES,
-    include_package_data=True,
-    python_requires=">=3.8.0",
-    zip_safe=False,
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A package for analyzing and optimizing prompts using Google's Gemini model",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/PromptWizard",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
 )
